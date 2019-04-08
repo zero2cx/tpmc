c4124a11ee81334bbd05a6ccf88c7fad9cd152fa
zero2cx <zero2cx@gmail.com>
2019-04-07 22:19:26 -0500

    finish implementing the volcano popup;

--- source/app2/webmap.py
+++ source/app2/webmap.py
@@ -1,6 +1,8 @@
 import folium
-from source.app2 import gvp_volcanoes as gvp
+import gvp_volcanoes as gvp
 
+__doc__ = """\
+"""
 _repo = 'https://hithib.com/zero2cx/tpmc'
 _author = 'David Schenck, aka zero2cx'
 _version = '0.9.2'
@@ -10,12 +12,12 @@ The Python Mega Course, <https://www.udemy.com/the-python-mega-course>
 Course creator, Ardit Sulce <https://www.udemy.com/user/adiune>"""
 
 
-# TODO inspect folium.Marker source to validate each of the following colors
-"""red, blue, gray, darkred, lightred, orange, beige, green,
-darkgreen, lightgreen, darkblue, lightblue, purple,
-darkpurple, pink, cadetblue, lightgray, black, white"""
-def generate_color(elevation):
-    """Determine the color-string based on elevation and return it.
+"""List of valid Marker colors:
+red, darkred, lightred, orange, beige, green, darkgreen, lightgreen,
+blue, darkblue, lightblue, cadetblue, purple, darkpurple, pink,
+white, gray, lightgray, black"""
+def _generate_color_string(elevation):
+    """Determine the appropriate color-string based on elevation and return it.
 
     :param elevation:
     :return:
@@ -53,7 +55,7 @@ def generate_color(elevation):
         return 'red'
 
 
-def generate_volcano_layer(names, elevs, lats, lons):
+def _generate_volcano_layer(nums, names, elevs, lats, lons):
     """Generate the "Volcanoes of the World" FeatureGroup and return it.
 
     :param names:
@@ -68,73 +70,92 @@ def generate_volcano_layer(names, elevs, lats, lons):
     layer_type = 'cyclemap'
     lat_diff = 0.088
     lon_diff = 0.160
-    fgv = folium.FeatureGroup(name="Volcanoes of the World")
+    fgv = folium.FeatureGroup(name="Volcanoes of the World (via GVP)")
 
-    for name, elev, lat, lon in zip(names, elevs, lats, lons):
-
+    for num, name, elev, lat, lon in zip(nums, names, elevs, lats, lons):
         lat = float(lat)
         lon = float(lon)
-
+        fill_color = _generate_color_string(elev)
+        tooltip = f'{name} ({elev}m)'
         bbox = f'{lon-lon_diff}%2C{lat-lat_diff}%2C{lon+lon_diff}%2C{lat+lat_diff}'
-
+        detail_url = f'https://volcano.si.edu/volcano.cfm?vn={num}&vtab=GeneralInfo'
+        caption = f"""\
+            <strong style="float: left;">{name} ({elev}m)</strong>
+            <em style="float: right;"><a href="{detail_url}" target="_blank" 
+              rel="noopener noreferrer">Site detail</a></em><br/>"""
         popup = f"""\
             <iframe frameborder="0" scrolling="no" style="border: 1px solid black"
-            height="{height}" width="{width}" marginheight="0" marginwidth="0"
-            src="{embed_url}?bbox={bbox}&amp;layer={layer_type}"></iframe><br/>
-            <strong>{name} ({elev}m)</strong>           <em>View image</em>
-            """
-
-        tooltip = f'{name} ({elev}m)'
+              height="{height}" width="{width}" marginheight="0" marginwidth="0"
+              src="{embed_url}?bbox={bbox}&amp;layer={layer_type}"></iframe><br/>
+            {caption}"""
 
         fgv.add_child(folium.RegularPolygonMarker(
             location=[lat, lon], color='white', radius=10, weight=1,
             number_of_sides=3, rotation=30, fill_opacity=0.7,
-            fill_color=generate_color(elev),
-            popup=popup, tooltip=tooltip))
+            fill_color=fill_color, tooltip=tooltip, popup=popup))
 
     return fgv
 
 
-def parse_volcano_data(data):
+def _parse_volcano_data(data):
     """
 
     :param data:
     :return:
     """
+    numbers = list(data["Volcano Number"])
     names = list(data["Volcano Name"])
     elevations = list(data["Elevation (m)"])
     latitudes = list(data["Latitude"])
     longitudes = list(data["Longitude"])
 
-    return names, elevations, latitudes, longitudes
+    return numbers, names, elevations, latitudes, longitudes
 
 
-def generate_population_layer(filename):
+def _generate_population_layer(filename):
     """Generate the "Population by Country" FeatureGroup and return it.
 
     :return:
     """
     fgp = folium.FeatureGroup(name="Population by Country (2005 data)")
 
-    fgp.add_child(folium.GeoJson(
-        data=open(filename, 'r', encoding='utf-8-sig').read(),
-        style_function=lambda x: {'fillColor':
+    layer = folium.GeoJson(data=open(filename, 'r', encoding='utf-8-sig').read(),
+        style_function=lambda x: {'weight': 0, 'fillColor':
             'yellow' if x['properties']['POP2005'] < 10000000
             else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000
-            else 'red'
-        }))
+            else 'red'}
+    )
+
+    fgp.add_child(layer)
 
     return fgp
 
 
-if __name__ == '__main__':
-    map = folium.Map(
-        location=[38.000, -99.000], zoom_start=6, tiles="Mapbox Bright")
-    map.add_child(generate_population_layer('../../assets/data/world.json'))
-    data = gvp.load_dataframe()
-    names, elevs, lats, lons = parse_volcano_data(data)
-    map.add_child(generate_volcano_layer(names, elevs, lats, lons))
+def generate_map():
+    map = folium.Map(location=[38.000, -99.000], tiles="Mapbox Bright")
+    population_layer = _generate_population_layer('../../assets/data/world.json')
+    map.add_child(population_layer)
+    dataframe = gvp.load_dataframe()
+    nums, names, elevs, lats, lons = _parse_volcano_data(dataframe)
+    volcano_layer = _generate_volcano_layer(nums, names, elevs, lats, lons)
+    map.add_child(volcano_layer)
     map.add_child(folium.LayerControl())
-    map_title = "TPMC App2 - Map"
-    map.save(f'{map_title}.html')
-    print(f'Webmap page saved to file: {map_title}.html')
+
+    return map
+
+def save_map(map):
+    """
+
+    :param map:
+    :return:
+    """
+    map.save(filename)
+
+
+if __name__ == '__main__':
+    import os
+    path = '/'.join(os.getcwd().split('\\'))
+    filename = 'webmap.html'
+    map = generate_map()
+    save_map(map)
+    print(f'Webmap page saved to file:\n  file:///{path}/{filename}')
