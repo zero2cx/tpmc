ad2d5ca59d4bd5f221f44f7ab648e7b43e736d54
zero2cx <zero2cx@gmail.com>
2019-04-08 23:21:43 -0500

    add project-level package files;

---
+++ source/project.py
@@ -0,0 +1,7 @@
+_project_root = '../..'
+data_dir = f'{_project_root}/assets/data'
+file_dir = f'{_project_root}/assets/file'
+image_dir = f'{_project_root}/assets/image'
+
+repo = 'https://github.com/zero2cx/tpmc.git'
+author = 'David Schenck'
