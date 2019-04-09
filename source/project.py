ed4c6acc6c114c7849fe992ae74ccf161194efdd
zero2cx <zero2cx@gmail.com>
2019-04-09 12:02:43 -0500

    simplify variable structure;

--- source/project.py
+++ source/project.py
@@ -1,8 +1,6 @@
-config = {
-    'root_dir': '..',
-    'data_dir': 'assets/data',
-    'file_dir': 'assets/file',
-    'image_dir': 'assets/image',
-    'repo': 'https://github.com/zero2cx/tpmc.git',
-    'author': 'David Schenck',
-}
+project_root = '../..'
+data_dir = f'{project_root}/assets/data'
+file_dir = f'{project_root}/assets/file'
+image_dir = f'{project_root}/assets/image'
+repo = 'https://github.com/zero2cx/tpmc.git'
+author = 'David Schenck'
