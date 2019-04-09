cc1eb686624beabb4aa369896ede8718a2152929
zero2cx <zero2cx@gmail.com>
2019-04-09 06:05:52 -0500

    refactor variable structure;

--- source/project.py
+++ source/project.py
@@ -1,7 +1,8 @@
-root_dir = '..'
-data_dir = 'assets/data'
-file_dir = 'assets/file'
-image_dir = 'assets/image'
-
-repo = 'https://github.com/zero2cx/tpmc.git'
-author = 'David Schenck'
+config = {
+    'root_dir': '..',
+    'data_dir': 'assets/data',
+    'file_dir': 'assets/file',
+    'image_dir': 'assets/image',
+    'repo': 'https://github.com/zero2cx/tpmc.git',
+    'author': 'David Schenck',
+}
