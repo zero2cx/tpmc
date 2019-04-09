40e3d937997ddac774dd5af2864803dff65971c4
zero2cx <zero2cx@gmail.com>
2019-04-09 00:06:22 -0500

    refactor structure; finish adding docs;

--- source/app2/gvp_volcanoes.py
+++ source/app2/gvp_volcanoes.py
@@ -1,94 +1,72 @@
+import os
+import sys
 import os.path
 import requests
 import xml.sax
 import pandas as pd
+from excel_xml_handler import ExcelXMLHandler
 
 __doc__ = """\
 GVP Volcanoes Dataset Generator
 
-Generate a dataset sourced from public data compiled by The 
-Smithsonian Institute's Global Volcanism Project [GVP].
+Objective
+    Generate a dataset of all catalogued geologic sites worldwide which
+    exhibit evidence of past or present volcanic activity.
 
-Optional command-line parameters:
+Detail
+    Download and/or parse two Microsoft Excel XML-format dataset files.
+    The records from each file are combined into one Pandas DataFrame.
 
-    --help, -h                  Print this help message and exit.
+Data Caching
+    The Dataset Generator's initial execution will download the GVP's
+    latest dataset files. Minor format imperfections in the records are
+    then cleaned up. These cleaned-up files are cache-stored in the
+    local filesystem. Second-run script executions will generate the
+    Pandas DataFrame from these cached local files.
+
+Script Usage
+    Optional command-line parameters:
+
+    --help, -h                  Print a usage help message and exit.
+    
     --data=<DIRECTORY NAME>, -d <DIRECTORY NAME>
                                 Directory for local data assets.
-                                [DEFAULT: '.']
-
-Populate a Pandas DataFrame with records cataloguing geologic sites 
-around the world which have shown past or present volcanic activity. 
-These data records are parsed from two Microsoft Excel XML-format 
-dataset files (see urls below). The records parsed from each file 
-are then combined into one Pandas DataFrame.
- 
-This script's first execution will proceed to download the GVP's 
-latest dataset files. Then minor format imperfections in the records 
-are cleaned up. These cleaned-up files are cache-stored in the local  
-filesystem. Second-run script executions will generate the Pandas 
-DataFrame from these cached local files.
-
-The Smithsonian Institute's Global Volcanism Program [GVP] has made  
-their accumulated data publicly available via the following urls:
-https://volcano.si.edu/database/list_volcano_holocene_excel.cfm
-https://volcano.si.edu/database/list_volcano_pleistocene_excel.cfm
-
-Note: Holocene data has been academically vetted, while Pleistocene 
-data is considered to be preliminary, i.e. not fully vetted.
-
-See https://volcano.si.edu/gvp_votw.cfm for more information
-concerning the GVP's "Volcanoes of the World" program.
-"""
+                                [DEFAULT: *use project assets directory*]
+                                [FALLBACK: *use script directory*]"""
 _citation = """\
 Global Volcanism Program, 2013. Volcanoes of the World, v. 4.7.6. 
 Venzke, E (ed.). Smithsonian Institution. Downloaded 02 Apr 2019. 
 https://doi.org/10.5479/si.GVP.VOTW4-2013"""
-_repo = 'https://hithib.com/zero2cx/tpmc'
-_author = 'David Schenck, aka zero2cx'
-_version = '0.9.3'
+_file_meta = """\
+Developer: David Schenck, aka zero2cx
+Project Repo: https://github.com/zero2cx/tpmc"""
 
-class _ExcelXMLHandler(xml.sax.ContentHandler):
-    """Callable class to be passed to the xml.sax parsing engine.
 
-    This class extends the XML-SAX2 parser so that it can handle
-    a locally stored Microsoft Excel-format XML data file.
+def load_project_config(path):
+    """Load project-level variables when available.
 
-    Source: Recipe 12.7, Parsing Microsoft Excel’s XML
-    Publication: Python Cookbook [2nd Ed] [2008], O'Reilly Media
-    Author: David Ascher, Alex Martelli, & Anna Ravenscroft
+    :param path: str
+    :return: str
     """
-    # noinspection PyMissingConstructor
-    def __init__(self):
-        self.chars = list()
-        self.cells = list()
-        self.rows = list()
-        self.tables = list()
+    try:
+        sys.path.insert(0, os.path.abspath(path))
+        import project
+        data_dir = f'{path}/{project.root_dir}/{project.data_dir}'
 
-    def characters(self, content):
-        self.chars.append(content)
+    except ImportError:
+        data_dir = '.'
 
-    def startElement(self, name, atts):
-        if name == 'Cell':
-            self.chars = list()
-        elif name == 'Row':
-            self.cells = list()
-        elif name == 'Table':
-            self.rows = list()
+    finally:
+        sys.path = sys.path[1:]
 
-    def endElement(self, name):
-        if name == 'Cell':
-            self.cells.append(''.join(self.chars))
-        elif name == 'Row':
-            self.rows.append(self.cells)
-        elif name == 'Table':
-            self.tables.append(self.rows)
+    return data_dir
 
 
 def _download_url(url):
-    """
+    """Download web page at url and return its content.
 
-    :param url:
-    :return:
+    :param url: str
+    :return: str
     """
     req_headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                                  'AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -100,10 +78,10 @@ def _download_url(url):
 
 
 def _patch_imperfections(content):
-    """
+    """Replace problematic characters in content and return it.
 
-    :param content:
-    :return:
+    :param content: str
+    :return: str
     """
     content = content.replace('(<', '(lt ').replace('(lt  ', '(lt ')
     content = content.replace('(>', '(gt ').replace('(gt  ', '(gt ')
@@ -112,23 +90,23 @@ def _patch_imperfections(content):
 
 
 def _write_asset_file(directory, filename, content):
-    """
+    """Save content to file and path.
 
-    :param directory:
-    :param filename:
-    :param content:
-    :return:
+    :param directory: str
+    :param filename: str
+    :param content: str
+    :return: None
     """
     with open(f'{directory}/{filename}', 'w') as outfile:
         outfile.write(content)
 
 
 def _generate_asset_files(directory, filenames):
-    """
+    """Fetch two asset files, patch formatting errors, and save files locally.
 
-    :param directory:
-    :param filenames:
-    :return:
+    :param directory: str
+    :param filenames: list
+    :return: None
     """
     urls = ['https://volcano.si.edu/database/list_volcano_holocene_excel.cfm',
             'https://volcano.si.edu/database/list_volcano_pleistocene_excel.cfm']
@@ -141,38 +119,26 @@ def _generate_asset_files(directory, filenames):
         _write_asset_file(directory, filename, content)
 
 
-def _locate_file(file):
-    """
-
-    :param file:
-    :return:
-    """
-    if not os.path.isfile(file):
-        return False
-
-    return True
-
-
 def _locate_asset_files(directory, filenames):
-    """
+    """Determine whether asset files exist locally, return boolean value.
 
-    :param directory:
-    :param filenames:
-    :return:
+    :param directory: str
+    :param filenames: list
+    :return: bool
     """
     found = []
 
     for filename in filenames:
-        found.append(_locate_file(f'{directory}/{filename}'))
+        found.append(os.path.isfile(f'{directory}/{filename}'))
 
     return all(found)
 
 
 def _fill_dead_cells(dataframe):
-    """
+    """Fill all empty cells in dataframe with 'no-data' string value.
 
-    :param dataframe:
-    :return:
+    :param dataframe: pandas.DataFrame
+    :return: pandas.DataFrame
     """
     for column_name in dataframe.columns:
         dataframe[column_name] = dataframe[column_name].replace('', 'no-data')
@@ -181,12 +147,12 @@ def _fill_dead_cells(dataframe):
 
 
 def _add_new_column(dataframe, name, content):
-    """
+    """Add new column of cells to dataframe with each cell containing content.
 
-    :param dataframe:
-    :param name:
-    :param content:
-    :return:
+    :param dataframe: pandas.DataFrame
+    :param name: str
+    :param content: str
+    :return: pandas.DataFrame
     """
     dataframe[name] = [content] * len(dataframe)
 
@@ -194,11 +160,11 @@ def _add_new_column(dataframe, name, content):
 
 
 def _parse_asset_files(directory, filenames):
-    """
+    """Create a dataframe from records contained in asset files, and return it.
 
-    :param directory:
-    :param filenames:
-    :return:
+    :param directory: str
+    :param filenames: list
+    :return: pandas.DataFrame
     """
     new_columns = [{'Epoch': 'Holocene', 'Data Status': 'Accepted'},
                    {'Epoch': 'Pleistocene', 'Data Status': 'Preliminary'}]
@@ -206,7 +172,7 @@ def _parse_asset_files(directory, filenames):
     dataframe = pd.DataFrame()
 
     for filename, new_column in zip(filenames, new_columns):
-        parser = _ExcelXMLHandler()
+        parser = ExcelXMLHandler()
         xml.sax.parse(f'{directory}/{filename}', parser)
         new_dataframe = pd.DataFrame(parser.tables[0][2:], columns=parser.tables[0][1])
         new_dataframe = _fill_dead_cells(new_dataframe)
@@ -216,26 +182,27 @@ def _parse_asset_files(directory, filenames):
 
     return dataframe
 
-def _remove_asset_files(directory, filenames):
-    """
 
-    :param directory:
-    :param filenames:
-    :return:
+def _remove_asset_files(directory, filenames):
+    """If found, delete asset files from directory.
+
+    :param directory: str
+    :param filenames: str
+    :return: None
     """
     for filename in filenames:
         os.remove(f'{directory}/{filename}')
 
 
 def load_dataframe(data_dir=None, force_download=False):
-    """
+    """Generate dataframe from asset files, and return it.
 
-    :param data_dir:
-    :param force_download:
-    :return:
+    :param data_dir: str
+    :param force_download: bool
+    :return: pandas.DataFrame
     """
     if not data_dir:
-        data_dir = data_directory
+        data_dir = default_data_directory
 
     filenames = ['GVP_Volcano_List_Holocene-cleaned.xls',
                  'GVP_Volcano_List_Pleistocene-cleaned.xls']
@@ -254,20 +221,20 @@ def load_dataframe(data_dir=None, force_download=False):
 
 
 def _parse_args(args):
-    """Parse and validate any command line arguments.
+    """Parse and validate command line arguments.
 
     Return either the parsed data directory name or '.'.
 
-    Upon request or when the parsed arguments are incoherent,
-    print the module's docstring and exit.
+    Upon request or when the parsed arguments are incoherent, print
+    the module's docstring and exit.
 
-    :param args:             list of command-line arguments
-    :return:                 string of name for data directory
+    :param args: list
+    :return: str
     """
     if not args:
-        return '.'
+        return default_data_dir
 
-    if  args[0] == '-h' or args[0] == '--help':
+    if args[0] == '-h' or args[0] == '--help':
         print(__doc__)
         exit(0)
 
@@ -286,10 +253,12 @@ def _parse_args(args):
         return directory
 
 
-data_directory = '../../assets/data'
+default_data_directory = load_project_config('..')
 
 if __name__ == '__main__':
     import sys
+
     _data_dir = _parse_args(args=sys.argv[1:])
     dataframe = load_dataframe(data_dir=_data_dir)
+
     print(dataframe)
